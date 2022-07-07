39ebeeb861fa5c7e27dcc6ea7d64c4d486bdb2b1
Felipe Rosas <felipe.rosas@g.globo>
2022-07-06 23:50:07 -0300

    chore: add share component

---
+++ src/components/EndGame/Share/StyledShare.ts
@@ -0,0 +1,47 @@
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+
+export const Container = styled(Box)`
+  color: #00888f;
+  display: flex;
+  font-size: 40px;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 60px;
+  width: 100%;
+`;
+
+export const ContainerIcons = styled(Box)`
+  display: flex;
+  margin: 0 auto;
+  text-align: center;
+  width: 240px;
+
+  .rounded-icon {
+    background-color: #00638c;
+    border-radius: 50%;
+    color: #f5f5f5;
+    padding: 6px;
+    font-size: 38px;
+  }
+`;
+
+export const ItemIcons = styled(Box)`
+  margin: 0 auto;
+`;
+
+export const Paragraph = styled(Box)`
+  color: #4e4e4e;
+  display: flex;
+  font-family: "Globotipo Rd", sans-serif;
+  font-size: 18px;
+  justify-content: center;
+  margin: 10px 0;
+  width: 100%;
+`;
+
+export const ShareLink = styled(Box)`
+  cursor: pointer;
+  text-decoration: underline;
+  padding-left: 5px;
+`;
---
+++ src/components/EndGame/Share/index.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import * as S from "./StyledShare";
+
+import FacebookIcon from "@mui/icons-material/FacebookRounded";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+
+interface ShareProps {
+  shareContent: string;
+  url: string;
+}
+const BASE_URL_FACEBOOK = "https://www.facebook.com/sharer/sharer.php?u=";
+const BASE_URL_WHATSAPP = "whatsapp://send?text=";
+const BASE_URL_TWITTER = "https://twitter.com/intent/tweet?text=";
+
+export const Share = ({ shareContent, url }: ShareProps) => {
+  const handleCopyText = () => {
+    navigator.clipboard.writeText(`${shareContent} ${url}`);
+  };
+
+  return (
+    <S.Container>
+      <S.Paragraph>Compartilhe o seu desafio:</S.Paragraph>
+
+      <S.ContainerIcons>
+        <S.ItemIcons>
+          <a
+            href={`${BASE_URL_FACEBOOK}${url}&t=${shareContent} `}
+            target="_blank"
+          >
+            <FacebookIcon className="rounded-icon" />
+          </a>
+        </S.ItemIcons>
+
+        <S.ItemIcons>
+          <a
+            href={`${BASE_URL_WHATSAPP}${shareContent} ${url} #NovelApp`}
+            target="_blank"
+          >
+            <WhatsAppIcon className="rounded-icon" />
+          </a>
+        </S.ItemIcons>
+        <S.ItemIcons>
+          <a
+            href={`${BASE_URL_TWITTER}${shareContent}&url=${url}&hashtags=NovelApp`}
+            target="_blank"
+          >
+            <TwitterIcon className="rounded-icon" />
+          </a>
+        </S.ItemIcons>
+      </S.ContainerIcons>
+
+      <S.Paragraph>
+        Para compartilhar em outras redes,
+        <S.ShareLink onClick={handleCopyText}>copie aqui.</S.ShareLink>
+      </S.Paragraph>
+    </S.Container>
+  );
+};
+
+export default Share;
