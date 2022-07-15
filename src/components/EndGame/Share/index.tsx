a7d098c71571820fe5a7ac7ce70aadef19c3bab1
Cesar Peres <cperes84@gmail.com>
2022-07-15 01:21:14 -0300

    feature: incluir popover com comportamento de texto copiado

--- src/components/EndGame/Share/index.tsx
+++ src/components/EndGame/Share/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import * as S from "./StyledShare";
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
 
 import FacebookIcon from "@mui/icons-material/FacebookRounded";
 import TwitterIcon from "@mui/icons-material/Twitter";
@@ -14,14 +16,23 @@ const BASE_URL_WHATSAPP = "whatsapp://send?text=";
 const BASE_URL_TWITTER = "https://twitter.com/intent/tweet?text=";
 
 export const Share = ({ shareContent, url }: ShareProps) => {
-  const handleCopyText = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleCopyText = (event: any) => {
     navigator.clipboard.writeText(`${shareContent} ${url}`);
+    setAnchorEl(event.currentTarget);
   };
+  
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   return (
     <S.Container>
       <S.Paragraph>Compartilhe o seu desafio:</S.Paragraph>
-
       <S.ContainerIcons>
         <S.ItemIcons>
           <a
@@ -52,7 +63,19 @@ export const Share = ({ shareContent, url }: ShareProps) => {
 
       <S.Paragraph>
         Para compartilhar em outras redes,
-        <S.ShareLink onClick={handleCopyText}>copie aqui.</S.ShareLink>
+        <S.ShareLink aria-describedby={id} onClick={handleCopyText}>copie aqui.</S.ShareLink>
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+        >
+          <Typography sx={{ p: 2 }}>Texto de compartilhamento copiado.</Typography>
+        </Popover>
       </S.Paragraph>
     </S.Container>
   );
