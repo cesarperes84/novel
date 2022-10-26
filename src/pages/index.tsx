e9e239f2e593e8508bc2e70bcd40af08a6badab2
cesarperes84 <cperes84@gmail.com>
2022-10-26 19:01:51 -0300

    feature: add google analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from "next";
+import React, { useEffect } from "react";
+
 import styles from "../styles/Home.module.css";
 import Main from "../components/Main";
 import Meta from "../components/Meta";
+import ReactGA from 'react-ga';
 
 const Home: NextPage = () => {
+
+  useEffect(() => {
+    ReactGA.initialize('G-YL86TQ1N49');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Meta />
